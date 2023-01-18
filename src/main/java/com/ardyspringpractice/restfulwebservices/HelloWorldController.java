08cd771892055c37cdbec25fcf0b09bb4f82e0b7
ardalangh <rdghoorchian@gmail.com>
2023-01-17 16:14:43 -0800

    better syntax for the controller mapping

--- src/main/java/com/ardyspringpractice/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/ardyspringpractice/restfulwebservices/HelloWorldController.java
@@ -1,13 +1,12 @@
 package com.ardyspringpractice.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // REST API
 @RestController
 public class HelloWorldController {
-    @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+    @GetMapping(path = "/helloworld")
     public String helloWorld() {
         return "Hello World!";
     }
