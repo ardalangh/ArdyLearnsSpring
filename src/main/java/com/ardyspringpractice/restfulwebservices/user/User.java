cdce9e7e13785683ffe426c24ea11a5708b7f396
ardalangh <rdghoorchian@gmail.com>
2023-01-18 09:30:10 -0800

    Static Filtering Json responses

--- src/main/java/com/ardyspringpractice/restfulwebservices/user/User.java
+++ src/main/java/com/ardyspringpractice/restfulwebservices/user/User.java
@@ -1,5 +1,7 @@
 package com.ardyspringpractice.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +10,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 char")
+    @JsonProperty("User_name")
     private String name;
     @Past(message = "Date of birth should be in the past")
+    @JsonProperty("birth_day")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
